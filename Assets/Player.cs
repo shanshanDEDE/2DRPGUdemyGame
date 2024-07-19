d18d275e7a7d166dc67a114ba98e2df9c114b241
willy <willyjen1111@gmail.com>
2024-07-19 20:22:39 +0800

    071922

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,29 +5,49 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
 
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
 
+    [SerializeField] private bool isMoving;
     private float xInput;
-    // Start is called before the first frame update
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         //rb.velocity = new Vector2(5, rb.velocity.y);
         xInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
-
+        Movement();
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            // rb.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+            Jump();
         }
+
+        AnimatorControllers();
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void AnimatorControllers()
+    {
+        isMoving = rb.velocity.x != 0;
+
+        anim.SetBool("isMoving", isMoving);
+
     }
 }
